8eff5811a387c45200f37e95cb4e72aec1c80b37
Onkar-kumbhar <onkarkumbhar@gmail.com>
2025-06-13 17:43:33 +0530

    fourth

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def root():
-  return "welcome to the flask app"
+  return "welcome to the flask app 1"
 
 
 app.run(host="0.0.0.0", port=3000, debug=True)
